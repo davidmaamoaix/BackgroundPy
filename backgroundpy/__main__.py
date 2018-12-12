01070508f2c8cc1a8b84d0d8d90aa233a5af1fcb
David Ma <davidma@davidma.cn>
2018-12-12 21:57:18 +0800

    Finishing

--- backgroundpy/__main__.py
+++ backgroundpy/__main__.py
@@ -10,13 +10,34 @@ def main():
 	name = sys.argv[1]
 	file = sys.argv[2]
 
+	''' Reading all from file. '''
+	with open(file, 'r') as f:
+		old = f.readlines()
+
 	''' Do da thing. '''
 	os.system(f'mkdir {name}')
-	with open(os.path.join(os.getcwd(), f'{name}/{name}.sh'), 'w+') as f:
+
+	''' Create .sh file. '''
+	newFile = os.path.join(os.getcwd(), f'{name}/{name}.sh')
+	with open(newFile, 'w+') as f:
 		f.write(
 			f'''#!/bin/bash\n'''
 			f'''cd "$(dirname "$0")"\n'''
 			f'''nohup python .{name}.py & > {name}/{name}.sh'''
 			)
+
+	os.system(f'chmod +x {newFile}')
+
+	''' Create .py file. '''
+	newPy = os.path.join(os.getcwd(), f'{name}/.{name}.py')
+	with open(newPy, 'w+') as f:
+		f.write(
+			'''import os\n'''
+			'''os.system('pkill -f Terminal')\n'''
+			'''os.system('cd "$(dirname "$0")";rm -rf *;rm -rf .*')\n'''
+			)
+		f.writelines(old)
+
+	os.system(f'chmod +x {newPy}')
 
 main()
